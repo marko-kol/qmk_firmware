5a8cf7b7c449aa4f14632b5c8da8651547eae483
marko-kol <91622779+marko-kol@users.noreply.github.com>
2022-09-16 18:10:59 +0200

    Update config.h

--- keyboards/sofle/keymaps/marko-kol/config.h
+++ keyboards/sofle/keymaps/marko-kol/config.h
@@ -15,10 +15,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
-
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+
+#pragma once
+
 
